bd3b3b46e3d3a69010c04a39b9e52782d36f2b0e
cr4yu <craayu@gmail.com>
2020-03-19 13:11:51 +0100

    more airports data

--- flights-service/src/main/resources/data.sql
+++ flights-service/src/main/resources/data.sql
@@ -1,11 +1,25 @@
 DROP TABLE IF EXISTS airport;
 
 CREATE TABLE airport (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
   city VARCHAR(250) NOT NULL
 );
 
 INSERT INTO airport (name, city) VALUES
-  ('London Stansted', 'London'),
-  ('Warsaw Modlin', 'Warsaw');
+    ('London Stansted', 'London'),
+    ('Warsaw Modlin', 'Warsaw'),
+    ('Berlin Tegel', 'Berlin'),
+    ('Warsaw Chopin', 'Warsaw'),
+    ('Krakow', 'Krakow'),
+    ('Gdansk Lech Walesa', 'Gdansk'),
+    ('Katowice', 'Katowice'),
+    ('Wroclaw', 'Wroclaw'),
+    ('Poznan', 'Poznan'),
+    ('Rzeszow International', 'Rzeszow'),
+    ('"Solidarity" Szczecin-Goleniow', 'Szczecin'),
+    ('Bydgoszcz', 'Bydgoszcz'),
+    ('Lublin', 'Lublin'),
+    ('Lodz', 'Lodz'),
+    ('Olsztyn-Mazury', 'Olsztyn'),
+    ('Zielona Gora', 'Zielona Gora');
