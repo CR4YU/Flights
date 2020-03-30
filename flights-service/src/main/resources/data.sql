e229596013c37ba21c657337cee658e7287e26e5
cr4yu <craayu@gmail.com>
2020-03-30 12:06:54 +0200

    sql data

--- flights-service/src/main/resources/data.sql
+++ flights-service/src/main/resources/data.sql
@@ -1,9 +1,21 @@
+DROP TABLE IF EXISTS flight;
 DROP TABLE IF EXISTS airport;
 
+
 CREATE TABLE airport (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  city VARCHAR(250) NOT NULL
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(250) NOT NULL,
+                         city VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE flight (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        origin_id INT NOT NULL,
+                        destination_id INT NOT NULL,
+                        departure DATETIME NOT NULL,
+                        arrival DATETIME NOT NULL,
+                        CONSTRAINT fk_origin_id FOREIGN KEY(origin_id) REFERENCES airport(id),
+                        CONSTRAINT fk_destination_id FOREIGN KEY(destination_id) REFERENCES airport(id)
 );
 
 INSERT INTO airport (name, city) VALUES
@@ -23,3 +35,16 @@ INSERT INTO airport (name, city) VALUES
     ('Lodz', 'Lodz'),
     ('Olsztyn-Mazury', 'Olsztyn'),
     ('Zielona Gora', 'Zielona Gora');
+
+INSERT INTO flight (origin_id, destination_id, departure, arrival) VALUES
+    (1, 2, parsedatetime('17-09-2020 23:05', 'dd-MM-yyyy hh:mm'), parsedatetime('18-09-2020 00:30', 'dd-MM-yyyy hh:mm')),
+    (1, 2, parsedatetime('18-09-2020 18:47', 'dd-MM-yyyy hh:mm'), parsedatetime('18-09-2020 19:47', 'dd-MM-yyyy hh:mm')),
+    (1, 2, parsedatetime('17-09-2020 6:30', 'dd-MM-yyyy hh:mm'), parsedatetime('17-09-2020 7:20', 'dd-MM-yyyy hh:mm')),
+    (1, 2, parsedatetime('17-09-2020 15:05', 'dd-MM-yyyy hh:mm'), parsedatetime('17-09-2020 16:30', 'dd-MM-yyyy hh:mm')),
+
+    (11, 3, parsedatetime('17-09-2020 23:05', 'dd-MM-yyyy hh:mm'), parsedatetime('18-09-2020 00:30', 'dd-MM-yyyy hh:mm')),
+    (11, 3, parsedatetime('18-09-2020 18:47', 'dd-MM-yyyy hh:mm'), parsedatetime('18-09-2020 19:47', 'dd-MM-yyyy hh:mm')),
+    (11, 3, parsedatetime('17-09-2020 6:30', 'dd-MM-yyyy hh:mm'), parsedatetime('17-09-2020 7:20', 'dd-MM-yyyy hh:mm')),
+    (11, 3, parsedatetime('17-09-2020 15:05', 'dd-MM-yyyy hh:mm'), parsedatetime('17-09-2020 16:30', 'dd-MM-yyyy hh:mm')),
+
+    (5, 7, parsedatetime('17-09-2012 18:47', 'dd-MM-yyyy hh:mm'), parsedatetime('17-09-2012 19:47', 'dd-MM-yyyy hh:mm'));
