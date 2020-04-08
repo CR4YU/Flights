848b0646f3018efb071cf50491481d064b02d07e
cr4yu <craayu@gmail.com>
2020-04-08 10:17:53 +0200

    new modules

---
+++ flights-service/src/main/flights-app/src/Main/Finder/Finder.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Finder.css'
+import Airports from "./Airports/Airports";
+import Flights from "./Flights/Flights";
+
+class Finder extends React.Component {
+
+    state = {
+        origin: '',
+        destination: ''
+    };
+
+    constructor() {
+        super();
+        this.handleSearchFlights = this.handleSearchFlights.bind(this);
+    }
+
+    handleSearchFlights = (origin, destination) => {
+        this.setState({origin: origin, destination: destination});
+    };
+
+    render = () => {
+        return (
+            <div className="Finder">
+                <Airports handleSearchFlights={this.handleSearchFlights}/>
+                <Flights origin={this.state.origin} destination={this.state.destination}/>
+            </div>
+        );
+    }
+}
+
+export default Finder;
---
+++ flights-service/src/main/flights-app/src/Main/SelectedFlight/SelectedFlight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SelectedFlight.css'
+
+class SelectedFlight extends React.Component {
+    render = () => {
+        return (
+            <div className="SelectedFlight">
+                Some Flight
+            </div>
+        );
+    }
+}
+
+export default SelectedFlight;
