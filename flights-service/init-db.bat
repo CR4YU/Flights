cc42bd8757a4ac1c61129c9286bdfa7574802d38
cr4yu <craayu@gmail.com>
2020-05-13 16:06:12 +0200

    changing database technology from h2 to dynamodb

--- flights-service/init-db.bat
+++ flights-service/init-db.bat
@@ -1,11 +1,25 @@
-aws dynamodb delete-table --table-name Flight --endpoint-url http://localhost:8000
-aws dynamodb delete-table --table-name Airport --endpoint-url http://localhost:8000
-aws dynamodb delete-table --table-name Booking --endpoint-url http://localhost:8000
-aws dynamodb delete-table --table-name User --endpoint-url http://localhost:8000
+@ECHO OFF
+IF "%~1"=="local" (
+    echo Test database selected
+    set db_env=--endpoint-url http://localhost:8000
+) ELSE IF "%~1"=="prod" (
+    echo Production database selected
+    set db_env=""
+) ELSE (
+    echo No database environment provided. Use 'local' or 'prod'
+    exit /b 1
+)
 
-aws dynamodb create-table --cli-input-json file://aws/create-table-flight.json --endpoint-url http://localhost:8000
-aws dynamodb create-table --cli-input-json file://aws/create-table-airport.json --endpoint-url http://localhost:8000
-aws dynamodb create-table --cli-input-json file://aws/create-table-booking.json --endpoint-url http://localhost:8000
-aws dynamodb create-table --cli-input-json file://aws/create-table-user.json --endpoint-url http://localhost:8000
+aws dynamodb delete-table --table-name Flight %db_env%
+aws dynamodb delete-table --table-name Airport %db_env%
+aws dynamodb delete-table --table-name Booking %db_env%
+aws dynamodb delete-table --table-name User %db_env%
 
-aws dynamodb batch-write-item --request-items file://aws/data.json --endpoint-url http://localhost:8000
+aws dynamodb create-table --cli-input-json file://aws/create-table-flight.json %db_env%
+aws dynamodb create-table --cli-input-json file://aws/create-table-airport.json %db_env%
+aws dynamodb create-table --cli-input-json file://aws/create-table-booking.json %db_env%
+aws dynamodb create-table --cli-input-json file://aws/create-table-user.json %db_env%
+
+
+
+aws dynamodb batch-write-item --request-items file://aws/data.json %db_env%
--- flights-service/src/main/java/com/example/flightsservice/AppConfig.java
+++ flights-service/src/main/java/com/example/flightsservice/AppConfig.java
@@ -2,6 +2,7 @@ package com.example.flightsservice;
 
 
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -9,15 +10,28 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
 public class AppConfig {
 
+    @Profile("dev")
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
+    public DynamoDBMapper devDynamoDBMapper() {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1"))
+                .build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
+
+        return dynamoDBMapper;
+    }
+
+    @Profile("prod")
+    @Bean
+    public DynamoDBMapper prodDynamoDBMapper() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
                 .build();
         DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
 
