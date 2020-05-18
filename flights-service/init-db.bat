75db993dd91bf93c013040eb75a806a1319520d7
cr4yu <craayu@gmail.com>
2020-05-18 11:51:27 +0200

    db script change

--- flights-service/init-db.bat
+++ flights-service/init-db.bat
@@ -10,16 +10,5 @@ IF "%~1"=="local" (
     exit /b 1
 )
 
-aws dynamodb delete-table --table-name Flight %db_env%
-aws dynamodb delete-table --table-name Airport %db_env%
-aws dynamodb delete-table --table-name Booking %db_env%
-aws dynamodb delete-table --table-name User %db_env%
-
-aws dynamodb create-table --cli-input-json file://aws/create-table-flight.json %db_env%
-aws dynamodb create-table --cli-input-json file://aws/create-table-airport.json %db_env%
-aws dynamodb create-table --cli-input-json file://aws/create-table-booking.json %db_env%
-aws dynamodb create-table --cli-input-json file://aws/create-table-user.json %db_env%
-
-
 
 aws dynamodb batch-write-item --request-items file://aws/data.json %db_env%
