9f3c96a33850e2d03d3abe5b6346c2bead343d03
cr4yu <craayu@gmail.com>
2020-05-18 15:59:03 +0200

    unit test

--- flights-service/src/test/groovy/com/example/flightsservice/service/BookingServiceSpec.groovy
+++ flights-service/src/test/groovy/com/example/flightsservice/service/BookingServiceSpec.groovy
@@ -1,16 +1,17 @@
 package com.example.flightsservice.service
 
+import com.example.flightsservice.api.body.BookingRequestBody
 import com.example.flightsservice.entity.Booking
+import com.example.flightsservice.entity.Flight
+import com.example.flightsservice.entity.User
 import com.example.flightsservice.repository.BookingRepository
 import spock.lang.Specification
 
 class BookingServiceSpec extends Specification {
 
-
     final static def EXAMPLE_ID = "10"
 
     BookingService bookingService
-
     BookingRepository bookingRepository;
     UserService userService;
     FlightService flightService;
@@ -24,26 +25,43 @@ class BookingServiceSpec extends Specification {
         bookingService.bookingRepository = bookingRepository
         bookingService.flightService = flightService
         bookingService.userService = userService
-
-        Booking booking = new Booking()
-        booking.id = EXAMPLE_ID
-
-        bookingRepository.findById(EXAMPLE_ID) >> booking
-
     }
 
     def "should find booking by id"() {
-        setup:
-        def booking
-
         when:
-        booking = bookingService.findById(EXAMPLE_ID)
+        def booking = bookingService.findById(EXAMPLE_ID)
 
         then:
+        1 * bookingRepository.findById(EXAMPLE_ID) >> { id -> new Booking(id: EXAMPLE_ID)}
         booking.id == EXAMPLE_ID
+
     }
 
-    def "should save booking" () {
+    def "should save booking from BookingRequestBody object" () {
+        setup:
+        def bookingRequestBody = exampleBookingRequestBody()
 
+        when:
+        def booking = bookingService.save(bookingRequestBody)
+
+        then:
+        1 * userService.findByLogin(_ as String) >> {String login -> new User(login: login)}
+        1 * flightService.findById(_ as String) >> {String id -> new Flight(id: id)}
+        1 * bookingRepository.save(_ as Booking) >> {Booking b -> b.id = EXAMPLE_ID; b}
+
+        booking.id == EXAMPLE_ID
+        booking.userLogin == bookingRequestBody.user
+        booking.seats == bookingRequestBody.getSeatsComaSeparated()
+        booking.flightId == bookingRequestBody.flightId
+        booking.bundle == bookingRequestBody.bundle
+    }
+
+    static def exampleBookingRequestBody() {
+        def bookingRequestBody = new BookingRequestBody()
+        bookingRequestBody.bundle = "basic"
+        bookingRequestBody.flightId = "1"
+        bookingRequestBody.user = "jrambo"
+        bookingRequestBody.seats = "1A,2C"
+        return bookingRequestBody
     }
 }
